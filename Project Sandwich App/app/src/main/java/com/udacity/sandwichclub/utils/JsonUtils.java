77a3d4b28d8ed7b8d408df0d8403a41d577f52dc
aman-alam11 <aman.alam11@yahoo.comh>
2018-04-28 17:02:18 -0400

    project 1 complete

--- Project Sandwich App/app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ Project Sandwich App/app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,12 @@
 package com.udacity.sandwichclub.utils;
 
+import android.text.TextUtils;
 import android.util.Log;
 import com.udacity.sandwichclub.model.Sandwich;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,21 +23,32 @@ public class JsonUtils {
         // This contains the "mainName" String and "alsoKnownAs" JSONArray
         JSONObject nameObject = mainJSON.getJSONObject("name");
         String mainName = nameObject.getString("mainName");
-        JSONArray alternateNamesJSONList = nameObject.getJSONArray("alsoKnownAs");
 
+        JSONArray alternateNamesJSONList = nameObject.getJSONArray("alsoKnownAs");
         // Change JSONArray to ArrayList
         List<String> altNamesList = new ArrayList<>(alternateNamesJSONList.length());
-        for (int i=0; i < alternateNamesJSONList.length(); i++){
-            altNamesList.add(alternateNamesJSONList.getString(i));
-        }
+            for (int i = 0; i < alternateNamesJSONList.length(); i++) {
+                String eachAltName = (alternateNamesJSONList.getString(i) != null) ?
+                                        alternateNamesJSONList.getString(i) : "";
+                if(!TextUtils.isEmpty(eachAltName)) {
+                    altNamesList.add(alternateNamesJSONList.getString(i));
+                }
+            }
+
 
         String placeOfOrigin = mainJSON.getString("placeOfOrigin");
         String descriptionOfood = mainJSON.getString("description");
         String urlImage = mainJSON.getString("image");
+
         JSONArray listOfIngredientsJSONArray = mainJSON.getJSONArray("ingredients");
         List<String> listOfIngredients = new ArrayList<>(listOfIngredientsJSONArray.length());
         for(int i=0; i < listOfIngredientsJSONArray.length(); i++ ){
-            listOfIngredients.add(listOfIngredientsJSONArray.getString(i));
+            String eachIngredient = (listOfIngredientsJSONArray.getString(i) != null) ?
+                                        listOfIngredientsJSONArray.getString(i) : "";
+
+            if(!TextUtils.isEmpty(eachIngredient)) {
+                listOfIngredients.add(eachIngredient);
+            }
         }
 
         return new Sandwich(mainName, altNamesList, placeOfOrigin, descriptionOfood, urlImage, listOfIngredients);
